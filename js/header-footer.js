5ddbc9846360eca7ebec7214a0481ea152ae16f6
Buddhika Prasanna <buddhikapbandara@gmail.com>
2017-10-19 15:04:59 +0530

    Initial Commit

---
+++ js/header-footer.js
@@ -0,0 +1,10 @@
+$(function(){
+            $("#header").load("header.html");
+            $("#footer").load("footer.html");
+            $("#product-sidebar").load("product-sidebar.html");
+            $("#services-sidebar").load("services-sidebar.html");
+            $("#cloud-sidebar").load("cloud-sidebar.html");
+            $("#training-sidebar").load("training-sidebar.html");
+            $("#mobile-header").load("mobile-header.html");
+});
+
